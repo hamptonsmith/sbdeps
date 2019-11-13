0f9a238e27c92db63d53cf8f19f08c9492a16fd5
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2019-11-13 16:57:47 -0500

    Basically working code.

---
+++ index.js
@@ -0,0 +1,60 @@
+const SBError = require('@shieldsbetter/sberror');
+
+const NoSuchFactory = SBError.subtype(
+        'NoSuchFactory', 'No factory called "{{name}}".');
+
+module.exports = class {
+    constructor() {
+        this.factoryEntries = {};
+        this.cachedSingletons = {};
+    }
+    
+    installSingleton(name, factory) {
+        installFactory(this, name, 'singleton', factory);
+    }
+    
+    install(name, factory) {
+        installFactory(this, name, 'instance', factory);
+    }
+    
+    get(name) {
+        const entry = this.factoryEntries[name];
+        
+        if (!entry) {
+            throw new NoSuchFactory({ name });
+        }
+        
+        const args = [...arguments].slice(1);
+        
+        let result;
+        switch (entry.type) {
+            case 'singleton': {
+                if (!this.cachedSingletons[name]) {
+                    this.cachedSingletons[name] =
+                            evaluate(this, entry.factory, args);
+                }
+                
+                result = this.cachedSingletons[name];
+                
+                break;
+            }
+            case 'instance': {
+                result = evaluate(this, entry.factory, args);
+                break;
+            }
+            default: {
+                throw new Error('Unknown entry type: ' + entry.type);
+            }
+        }
+        
+        return result;
+    }
+};
+
+function evaluate(sbd, factory, args) {
+    return factory(sbd, ...args);
+}
+
+function installFactory(sbd, name, type, factory) {
+    sbd.factoryEntries[name] = { type, factory };
+}
